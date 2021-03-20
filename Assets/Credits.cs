0708ad2ce12064686ad3f9561a23edba5073fbad
Darian Nguyen <17971850+nguyen-darianhuy@users.noreply.github.com>
2021-03-20 01:02:11 -0700

    Implement Credits Scene

---
+++ Assets/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Quit());
+    }
+
+    IEnumerator Quit()
+    {
+        yield return new WaitForSeconds(120f);
+        SteamVR_Fade.View(Color.black, 2);
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
